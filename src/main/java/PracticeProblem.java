a8374a756266fcb45d3ae005bd3880b34ef22a44
KavinKumaran123 <166553090+KavinKumaran123@users.noreply.github.com>
2026-03-02 00:18:28 +0000

    15

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,6 +2,35 @@ public class PracticeProblem {
 
 	public static void main(String args[]) {
 
-	}
+	}public static int goodString(String s) {
+    int n = s.length();
+    if (n < 2) return -1;
+
+
+    int[] first = new int[256];
+    int[] last = new int[256];
+
+
+    for (int i = 0; i < 256; i++) first[i] = -1;
+
+
+    for (int i = 0; i < n; i++) {
+        int ch = s.charAt(i);
+        if (first[ch] == -1) first[ch] = i;
+        last[ch] = i;
+    }
+
+    int best = Integer.MAX_VALUE;
+
+
+    for (int ch = 0; ch < 256; ch++) {
+        if (first[ch] != -1 && last[ch] > first[ch]) {
+            int ops = first[ch] + (n - 1 - last[ch]);
+            if (ops < best) best = ops;
+        }
+    }
+
+    return (best == Integer.MAX_VALUE) ? -1 : best;
+}
 
 }
